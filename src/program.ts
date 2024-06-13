572c4a247fcca713fef9ca70a337026c00617353
Solomon Hawk <solomon.hawk@viget.com>
2024-06-13 12:40:17 -0400

    Add vitest

--- src/program.ts
+++ src/program.ts
@@ -1,3 +1,21 @@
 import { Effect } from "effect";
 
 export const program = Effect.succeed(1);
+
+export type Position = {
+  x: number;
+  y: number;
+};
+
+export type Rover = {
+  position: Position;
+  direction: "N" | "S" | "E" | "W";
+};
+
+export type Command = "f" | "b" | "l" | "r";
+
+export type Planet = {
+  width: number;
+  height: number;
+  obstacles: Position[];
+};
