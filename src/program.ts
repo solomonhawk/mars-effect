2b089dcf1a26fc2ed77ca6c0100e13679dec955f
Solomon Hawk <solomon.hawk@viget.com>
2024-06-13 13:25:21 -0400

    Super basic forward move effect

--- src/program.ts
+++ src/program.ts
@@ -1,4 +1,5 @@
 import { Effect } from "effect";
+import * as Ref from "effect/Ref";
 
 export const program = Effect.succeed(1);
 
@@ -19,3 +20,45 @@ export type Planet = {
   height: number;
   obstacles: Position[];
 };
+
+class ObstacleError extends Error {
+  _tag = "ObstacleError";
+
+  constructor(readonly message: string) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
+
+export function runCommands(
+  rover: Ref.Ref<Rover>,
+  planet: Planet,
+  commands: Command[],
+): Effect.Effect<Rover, ObstacleError> {
+  return Effect.gen(function* (_) {
+    let currentRover = yield* rover.get;
+
+    for (const c of commands) {
+      currentRover = yield* runCommand(currentRover, planet, c);
+      Ref.set(rover, currentRover);
+    }
+
+    return yield* rover.get;
+  });
+}
+
+export function runCommand(
+  rover: Rover,
+  planet: Planet,
+  command: Command,
+): Effect.Effect<Rover, ObstacleError> {
+  switch (command) {
+    case "f":
+      return Effect.succeed({
+        ...rover,
+        position: { ...rover.position, y: rover.position.y + 1 },
+      });
+    default:
+      return Effect.succeed(rover);
+  }
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,35 @@
-describe("test", () => {
-  it("works", () => {
-    expect(true).toBe(true);
+import { Planet, Rover, runCommands } from "~/program";
+import { Effect } from "effect";
+import * as Ref from "effect/Ref";
+
+describe("without any obstacles", () => {
+  it("follows a forward command", async () => {
+    const rover: Rover = {
+      direction: "N",
+      position: { x: 0, y: 0 },
+    };
+
+    const planet: Planet = {
+      width: 5,
+      height: 5,
+      obstacles: [],
+    };
+
+    const roverRef = Ref.unsafeMake(rover);
+
+    const _result = await Effect.runPromise(
+      runCommands(roverRef, planet, ["f"]).pipe(
+        Effect.catchTag("ObstacleError", (error) => {
+          console.log(`Recovering from error ${error}`);
+          return roverRef.get;
+        }),
+      ),
+    );
+
+    roverRef.get.pipe(
+      Effect.tap((r) => {
+        expect(r.position).toEqual({ x: 0, y: 1 });
+      }),
+    );
   });
 });
